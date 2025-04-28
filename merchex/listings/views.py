1cd2e966edb6c7a1acdcea2d8e48bf7d33da0905
Leo-Brd <leobordet.pro@gmail.com>
2025-04-28 15:50:05 +0200

    Add first views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def hello(request):
+    return HttpResponse('<h1>Hello Django!</h1>')
+
+def about(request):
+    return HttpResponse('<h1>À propos</h1> <p>Nous adorons merch !</p>')
+
+def listings(request):
+    return HttpResponse('<ul><li><article>Article 1</article></li><li><article>Article 2</article></li><li><article>Article 3</article></li></ul>')
+
+def contactUs(request):
+    return HttpResponse('<h1>Formulaire de contact</h1><form></form>')
