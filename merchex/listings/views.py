c5d5693ded906d63dc81d45efde61fa7d30344a0
Leo-Brd <leobordet.pro@gmail.com>
2025-04-29 16:27:47 +0200

    Add list and details views

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,16 +5,28 @@ from listings.models import Band
 from listings.models import Listing
 
 
-def hello(request):
+def band_list(request):
     bands = Band.objects.all()
-    return render(request, 'listings/hello.html', {'bands': bands})
+    return render(request, 'listings/band_list.html', {'bands': bands})
+
+def band_detail(request, id):
+   band = Band.objects.get(id=id)
+   return render(request,
+          'listings/band_detail.html',
+         {'band': band})
 
 def about(request):
     return render(request, 'listings/about.html')
 
 def listings(request):
     listings = Listing.objects.all()
-    return render(request, 'listings/listings.html', {'listings': listings})
+    return render(request, 'listings/listings_list.html', {'listings': listings})
+
+def listing_detail(request, id):
+   listing = Listing.objects.get(id=id)
+   return render(request,
+          'listings/listing_detail.html',
+         {'listing': listing})
 
 def contactUs(request):
     return render(request, 'listings/contact.html')
