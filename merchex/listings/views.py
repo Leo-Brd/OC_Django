40887abd457b7d5b962091ba158a2e2de83f49ac
Leo-Brd <leobordet.pro@gmail.com>
2025-04-29 20:40:30 +0200

    Add first django form

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
 from listings.models import Band
 from listings.models import Listing
+from listings.forms import ContactUsForm
 
 
 def band_list(request):
@@ -10,10 +13,10 @@ def band_list(request):
     return render(request, 'listings/band_list.html', {'bands': bands})
 
 def band_detail(request, id):
-   band = Band.objects.get(id=id)
-   return render(request,
-          'listings/band_detail.html',
-         {'band': band})
+    band = Band.objects.get(id=id)
+    return render(request,
+        'listings/band_detail.html',
+        {'band': band})
 
 def about(request):
     return render(request, 'listings/about.html')
@@ -23,10 +26,26 @@ def listings(request):
     return render(request, 'listings/listings_list.html', {'listings': listings})
 
 def listing_detail(request, id):
-   listing = Listing.objects.get(id=id)
-   return render(request,
-          'listings/listing_detail.html',
-         {'listing': listing})
+    listing = Listing.objects.get(id=id)
+    return render(request,
+        'listings/listing_detail.html',
+        {'listing': listing})
 
-def contactUs(request):
-    return render(request, 'listings/contact.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+
+        if form.is_valid():
+            send_mail(
+            subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'],)
+            return redirect('/')
+    else:
+        form = ContactUsForm()
+
+    return render(request,
+                'listings/contact.html',
+                {'form': form})
